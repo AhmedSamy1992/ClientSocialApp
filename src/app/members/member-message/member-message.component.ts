fa0e4113608853867c7af6d89a0cc17bfdf2836c
Ahmed.Samy <Ahmed.Samy@DEV16-PC2>
2024-12-02 23:51:55 +0200

    fix message scroll

--- src/app/members/member-message/member-message.component.ts
+++ src/app/members/member-message/member-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, OnInit, output, Output, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, inject, input, OnInit, output, Output, ViewChild } from '@angular/core';
 import { MessageService } from '../../_services/message.service';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TimeagoModule } from 'ngx-timeago';
@@ -11,9 +11,9 @@ import { Message } from '../../_modules/message';
   templateUrl: './member-message.component.html',
   styleUrl: './member-message.component.css'
 })
-export class MemberMessageComponent{
+export class MemberMessageComponent implements AfterViewChecked{
   @ViewChild('messageForm') messageForm?: NgForm;
-  // @ViewChild('scrollMe') scrollContainer?: any;
+  @ViewChild('scrollMe') scrollContainer?: any;
   messageService = inject(MessageService);
   username = input.required<string>();
   messageContent = '';
@@ -27,8 +27,20 @@ export class MemberMessageComponent{
       next: message => {
         this.updateMessage.emit(message);
         this.messageForm?.reset();
+        this.scrollToBottom();
       }
     })
   }
+
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom() {
+    if (this.scrollContainer) {
+      this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+    }
+  }
+
 
 }
